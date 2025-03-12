7389952349ea2688db91e0a673a7410e45809cd8
屈畅 <chang@quchangdeMacBook-Pro.local>
2025-03-12 20:09:02 +1100

    修改测试环境

--- main.js
+++ main.js
@@ -2,31 +2,25 @@ import * as THREE from './js/three.module.js';
 import { OrbitControls } from './js/OrbitControls.js';
 import { GLTFLoader } from './js/GLTFLoader.js';
 
-// 后续你的 three.js 场景代码
-
-
-// 创建场景
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x222222);
 document.body.appendChild(renderer.domElement);
 
-// 控制器
 const controls = new OrbitControls(camera, renderer.domElement);
 const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
 
-// 测试立方体
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// 渲染循环
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
