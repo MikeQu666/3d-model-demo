e01e1ea648c8402cb99c899759131be0ca9fe5a9
屈畅 <chang@quchangdeMacBook-Pro.local>
2025-03-12 20:32:19 +1100

    修改测试环境

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.150.1';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.150.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.150.1/examples/jsm/loaders/GLTFLoader.js';
 
-
+// 创建场景
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5);
