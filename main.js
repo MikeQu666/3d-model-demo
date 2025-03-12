56d6a71a8931ccec213862a7030bb6956344d419
屈畅 <chang@quchangdeMacBook-Pro.local>
2025-03-12 19:54:49 +1100

    修改测试环境

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
-import { OrbitControls } from './js/OrbitControls.js';
-import { GLTFLoader } from './js/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
